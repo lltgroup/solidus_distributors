3d2d581f883bbc4748ebaaeb9d02fc824324ab2f
Kevin Voshell <kevin@llt-group.com>
2020-03-03 10:15:34 -0600

    add migration for distributor table

---
+++ db/migrate/create_spree_distributors_table.rb
@@ -0,0 +1,10 @@
+class CreateSpreeDistributorTables < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spree_distributors do |t|
+      t.string     :name, null: false
+      t.string     :email, null: false
+
+      t.timestamps
+    end
+  end
+end
