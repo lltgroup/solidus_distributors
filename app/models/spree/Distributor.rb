e2e25ee89cde097e094915da70c5b1e031be8a15
Kevin Voshell <kevin@llt-group.com>
2020-03-03 13:01:31 -0600

    add basic validations to distributor, make association optional

--- app/models/spree/Distributor.rb
+++ app/models/spree/Distributor.rb
@@ -1,5 +1,10 @@
 module Spree
   class Distributor < ActiveRecord::Base
     has_many :products, class_name: 'Spree::Product'
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    
+    validates :name, presence: true, length: { minimum: 1, maximum: 150 }
+    validates :email, format: {with: VALID_EMAIL_REGEX, message: "must be a valid email"}
   end
 end
--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,5 +1,5 @@
 module Spree
   Product.class_eval do
-    belongs_to :distributor, class_name: 'Spree::Distributor'
+    belongs_to :distributor, class_name: 'Spree::Distributor', optional: :true
   end
 end
